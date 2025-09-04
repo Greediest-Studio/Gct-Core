548f44bd13b55a9dd6640bd54a9d8ac9be554c02
mc_Edwin <yxy2439642228@163.com>
2025-09-04 09:00:02 +0800

    Revert "加上分离逻辑，移除召唤师的修改"
    This reverts commit 349bd7790f2d28ab9976895449ec3ddec48f2a46.

--- src/main/java/com/smd/gctcore/gctcore.java
+++ src/main/java/com/smd/gctcore/gctcore.java
@@ -1,7 +1,15 @@
 package com.smd.gctcore;
 
+import com.smd.gctcore.events.EventHooks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class gctcore {
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new EventHooks());
+    }
 }
