88aa71aa62123fef429a08e13d99037d84f84308
6666614li <2157995671a@gmail.com>
2025-10-24 00:35:50 +0800

    永昼永夜

--- src/main/java/com/smd/gctcore/world/AirportDim/WorldProviderAirport.java
+++ src/main/java/com/smd/gctcore/world/AirportDim/WorldProviderAirport.java
@@ -8,6 +8,18 @@ import net.minecraft.world.gen.IChunkGenerator;
 
 public class WorldProviderAirport extends WorldProvider {
 
+    private static final long Freeze_time = 6000L;
+    private boolean timeLocked = true;
+
+    @Override
+    public long getWorldTime() {
+        return timeLocked ? Freeze_time : world.getWorldTime();
+    }
+
+    public void lockTimeAtNoon() {
+        this.timeLocked = true;
+    }
+
     @Override
     public DimensionType getDimensionType() {
         return DimensionTypeAirport.Airport;
--- src/main/java/com/smd/gctcore/world/NothingnessDim/WorldProviderNothingness.java
+++ src/main/java/com/smd/gctcore/world/NothingnessDim/WorldProviderNothingness.java
@@ -8,6 +8,18 @@ import net.minecraft.world.gen.IChunkGenerator;
 
 public class WorldProviderNothingness extends WorldProvider {
 
+    private static final long Freeze_time = 18000L;
+    private boolean timeLocked = true;
+
+    @Override
+    public long getWorldTime() {
+        return timeLocked ? Freeze_time : world.getWorldTime();
+    }
+
+    public void lockTimeAtNoon() {
+        this.timeLocked = true;
+    }
+
     @Override
     public DimensionType getDimensionType() {
         return DimensionTypeNothingness.nothingness;
